6145c3f3d7b98f3035b0da343dc5433086ea3c7f
Sef Kloninger <sefklon@gmail.com>
2020-05-23 11:13:37 -0700

    Switch from bootstrap3 to bootstrap4
    Also update a variable name based on a warning.

--- conf.py
+++ conf.py
@@ -131,7 +131,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+THEME = "bootstrap4"
 
 # Below this point, everything is optional
 
@@ -934,7 +934,7 @@ BODY_END = """
 
 # If you hate "Filenames with Capital Letters and Spaces.md", you should
 # set this to true.
-UNSLUGIFY_TITLES = True
+FILE_METADATA_UNSLUGIFY_TITLES = True
 
 # Additional metadata that is added to a post when creating a new_post
 # ADDITIONAL_METADATA = {}
